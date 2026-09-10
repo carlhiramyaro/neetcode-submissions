bb4d352a2aa983d90e6ec9e1440013a231d5f7b9
carlhiramyaro <96393929+carlhiramyaro@users.noreply.github.com>
2026-09-10 12:46:48 +0000

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+
+        // 
+        // Hashhmap to count the appearance of each number in the array(counter)
+        // init a bucket(initiate an array of arrayList)
+        // for each number in the counter append the number to the array at its index in bucket
+        // iterate through bucket in the reverse order appending a result list until its length equals k
+        HashMap<Integer, Integer> counter = new HashMap<>(); 
+        for(Integer num:nums){
+            counter.put(num, counter.getOrDefault(num,0) + 1);
+        }
+        // System.out.println(counter.toString());
+
+
+        ArrayList<ArrayList<Integer>> buckets = new ArrayList<>();
+        for(int i = 0; i <= nums.length; i ++){
+            buckets.add(new ArrayList<>());
+        }
+
+        for(Map.Entry<Integer, Integer> mapElement: counter.entrySet()){
+            buckets.get(mapElement.getValue()).add(mapElement.getKey());
+        }
+
+        // System.out.println(buckets.toString());
+
+        int[] res = new int[k];
+        int index = 0;
+
+        for(int i = buckets.size() - 1; i >= 0; i --){
+            for(int num: buckets.get(i)){
+                res[index] = num;
+                index++;
+                if(index == k){
+                    return res;
+                }
+            }
+        }
+        return null;
+
+
+
+    
+
+        
+    }
+}
