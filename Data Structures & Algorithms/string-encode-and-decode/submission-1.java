598be42bd897e31590c6cf6f2b284d7d0b1fd35d
carlhiramyaro <96393929+carlhiramyaro@users.noreply.github.com>
2026-09-17 16:02:05 +0000

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.java
@@ -0,0 +1,58 @@
+class Solution {
+
+/*
+encode:
+initiate a res string
+for each word in the list we want to get the length of the word:
+concatenate the length, thsymbol #(indicating the start of a new word), and the word. to the res string
+return the res string.
+
+decode:
+2 pointer approach
+initiate a res array and a pointer variable(i)
+initiate another pointer j starts at i, j moves foward until it meets # meaning start of a word
+now that i is on the number and j is at #, the substring of i and j would be the length
+of the word.
+update pointers to capture the word:
+i becomes the start of the word (j + 1)
+j becomes the end of the word(i + wordLength)
+append the word(substring of i and j) to the res array
+i becomes j(end of the word and next number)
+
+
+
+
+*/
+    public String encode(List<String> strs) {
+        StringBuilder encodedString = new StringBuilder();
+        for(String word:strs){
+            encodedString.append(word.length());
+            encodedString.append('#');
+            encodedString.append(word);
+        }
+        return encodedString.toString();
+        
+
+    }
+
+    public List<String> decode(String str) {
+        ArrayList<String> decodedList = new ArrayList<>();
+        int i = 0;
+
+        while(i < str.length()){
+            int j = i;
+            while(str.charAt(j) != '#'){
+                j ++;
+            }
+            int wordLength = Integer.parseInt(str.substring(i,j));
+            i = j + 1;
+            j = i + wordLength;
+            decodedList.add(str.substring(i, j));
+            i = j;
+
+        }
+
+        return decodedList;
+       
+    }
+}
